65bb8d491a49f8bd87ff0091b5312cfaf53db001
dioferley-code <78866514+dioferley-code@users.noreply.github.com>
2025-08-24 15:36:59 -0500

    Ajuste login y iconos menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/common/login/login.module').then( m => m.LoginPageModule)
-  },  {
+  },
+  {
     path: 'products',
     loadChildren: () => import('./pages/products/products.module').then( m => m.ProductsPageModule)
   },
