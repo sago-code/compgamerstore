6ae5cadc8e2891d859dd4287d41a119c28683620
sago-code <139510724+sago-code@users.noreply.github.com>
2025-09-06 10:39:12 -0500

    arreglos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     apiKey: "AIzaSyDLDPIpYWWvV1lxPO706ArWXKND1hUES1E",
     authDomain: "compgamerstore.firebaseapp.com",
     projectId: "compgamerstore",
-    storageBucket: "compgamerstore.appspot.com",
+    storageBucket: "compgamerstore.firebasestorage.app",
     messagingSenderId: "840480061585",
     appId: "1:840480061585:web:be32259393d737f7512730",
     measurementId: "G-0KTBVD7V8G"
